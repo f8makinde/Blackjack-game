8cb8dd6b16f9fcfd4ab5fbd1abb1319c4d11970c
Faith Makinde <faithmakinde0@gmail.com>
2022-08-10 19:08:10 +0100

    dom manipulation

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 let firstNum = 6;
 let secondNum = 7;
 let sum = firstNum + secondNum;
+let message = "";
+let roundEl = document.getElementById("round");
+function start(){
+    if(sum < 21){
+        message = "You can draw more cards";
+       }
+       else if(sum === 21){
+           message = "You have won blackjack, congrats";
+       }
+       else{
+           message = "You have lost, sorry";
+       }
+    roundEl.textContent = message
+}
