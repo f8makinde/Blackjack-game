15320f15d5f4d389c00bf1a65328f170e7b32a0a
Faith Makinde <faithmakinde0@gmail.com>
2022-08-16 04:04:20 +0100

    added a new card button

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-let firstNum = 6;
-let secondNum = 7;
-let sum = firstNum + secondNum;
+let firstCard = 11;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
 let message = "";
-let roundEl = document.getElementById("round");
+let roundEl = document.querySelector("#round");
+let sumEl = document.querySelector("#sum");
+let cardsEl = document.getElementById("card");
+
 function start(){
+    renderGame();
+}
+function renderGame(){
     if(sum < 21){
-        message = "You can draw more cards";
-       }
-       else if(sum === 21){
-           message = "You have won blackjack, congrats";
-       }
-       else{
-           message = "You have lost, sorry";
-       }
-    roundEl.textContent = message
+        message = `Do you want to draw a new card`;
+        }
+        else if(sum === 21){
+            message = `Wooah you have won blackjack`;
+            hasBlackJack = true;
+        }
+        else{
+            message = `Sorry you have lost the game`;
+            isAlive = false;
+        }
+        roundEl.textContent = message;
+        sumEl.innerHTML = `Sum: ${sum}`;
+        cardsEl.innerHTML = `Card: ${firstCard} ${secondCard}`;
+}
+function newCard(){
+    let card = 2;
+    sum += card;
+    renderGame();
 }
