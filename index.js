c72739b68250b23df35143bb251503fdc09b0839
Faith Makinde <faithmakinde0@gmail.com>
2022-08-23 13:42:23 +0100

    bug fixed

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ let sumEl = document.querySelector("#sum");
 let cardsEl = document.getElementById("card");
 let playerEl = document.getElementById("player-el");
 
+playerEl.textContent = `${players.name}: $${players.money}`
+
 function getRandomNumber(){
     let randomNum = Math.floor(Math.random() * 13) + 1;
-    return randomNum
+     if(randomNum > 10){
+        return 10;
+     }
+     else if(randomNum === 1){
+        return 11;
+     }
+     else{
+        return randomNum;
+     }
 }
 function start(){
     isAlive = true;
@@ -26,26 +36,25 @@ function start(){
     renderGame();
 }
 function renderGame(){
-    sumEl.innerHTML = `Sum: ${sum}`;
-    cardsEl.textContent = `Card: `
+    cardsEl.textContent = "Cards: "
     for(let i = 0; i < allCards.length; i++){
-        cardsEl.textContent += `${allCards[i]} `;
+        cardsEl.textContent += allCards[i] + " ";
     }
+    sumEl.textContent = `Sum: ${sum}`;
     if(sum < 21){
-        message = `Do you want to draw a new card`;
+        message = "Do you want to draw a new card?";
         }
         else if(sum === 21){
-            message = `Wooah you have won blackjack`;
+            message = "Wooah you have won blackjack, click reset to start game again";
             hasBlackJack = true;
             players.money = 200;
         }
         else{
-            message = `Sorry you have lost the game`;
+            message = `Sorry you have lost the game, click reset to start game again`;
             isAlive = false;
             players.money = 0;
         }
-        roundEl.textContent = message;
-        playerEl.textContent = `${players.name}: $${players.money}`
+        roundEl.textContent = message;    
     
 }
 function newCard(){
@@ -66,81 +75,3 @@ function reset(){
 }
 
 
-// let allCards = []
-// let sum = 0;
-// let hasBlackJack = false;
-// let isAlive = false;
-// let message = "";
-// let roundEl = document.querySelector("#round");
-// let sumEl = document.querySelector("#sum");
-// let cardsEl = document.getElementById("card");
-// document.getElementById("name-el").value;
-// let x = document.getElementById("my-text")
-
-
-// function getRandomNumber(){
-//     let randomNum = Math.floor(Math.random() * 13) + 1;
-//     return randomNum
-// }
-// function start(){
-//     isAlive = true;
-//     let firstCard = getRandomNumber();
-//     let secondCard = getRandomNumber();
-//     allCards = [firstCard, secondCard];
-//     sum = firstCard + secondCard;
-//     renderGame();
-// }
-// function renderGame(){
-//     roundEl.textContent = message;
-//     sumEl.innerHTML = `Sum: ${sum}`;
-//     cardsEl.textContent = `Card: `
-//     for(let i = 0; i < allCards.length; i++){
-//         cardsEl.textContent += `${allCards[i]} `;
-//     } 
-//     if(sum < 21){
-//         message = `Do you want to draw a new card`;
-//         }
-//         else if(sum === 21){
-//             message = `Wooah you have won blackjack`;
-//             hasBlackJack = true;
-//         }
-//         else {
-//             message = `Sorry you have lost the game`;
-//             isAlive = true;
-//         }       
-//    }
-// function newCard(){
-//     if(isAlive === true && hasBlackJack === false){
-//         let card = getRandomNumber();
-//         sum += card;
-//         allCards.push(card);
-//         renderGame();
-//     }
-// }
-
-
-// let score = "";
-// function allName(){
-//     if(sum === 0){
-//         score = "";
-//     }
-//    else if(sum < 21){
-//         score = 100 + "💵🙂";
-//     }
-//     else if(sum === 21){
-//         score = 200 + "🤑💵💰🥳";
-//     }
-//     else if (sum > 21){
-//         score = 0 + "😭😭";
-//     }
-
-//     x.innerHTML = document.getElementById("name-el").value + " $" + score;
-// }
-// function reset(){
-//     sumEl.innerHTML = "";
-//     cardsEl.textContent = "";
-//     x.innerHTML = "";
-//     document.getElementById("name-el").value = "";
-//     score = "";
-//     roundEl.textContent = "Draw a new card";
-// }
